d2908ffddd7176a94b7e4de4b2eec3a278e24f51
llllllllll <joejev@gmail.com>
2013-11-16 17:29:18 -0500

    finished c solution.

--- src/test.c
+++ src/test.c
@@ -1,21 +1,40 @@
 // Joe Jevnik
+// 16.11.2013
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
+#include <gmp.h>
 
 typedef unsigned int u_int;
+typedef unsigned long long int u_llong;
 
-extern u_int pairs[][] = {{1,1},{2,2},{5,5},{6,9},{8,8},{9,6}};
+bool is_flippable(char*);
+bool is_found(char*);
+char *find_n(char**,u_int);
 
-extern u_int flippables[6] = {1,2,5,6,8,9};
+// The flippable pairs.
+static char *pairs[] = {"00","11","22","55","69","88","96"};
 
-bool is_flippable(u_int num[],u_int digits){
+// The flippable characters
+static char flippables[] = "01235689";
+
+// Returns true if the number is flippable.
+bool is_flippable(char *str){
+    mpz_t n;
     bool f;
+    char b[32];
+    u_int digits;
+    mpz_init_set_ui(n,atoi(str));
+    mpz_mul(n,n,n);
+    digits = gmp_sprintf(b,"%Zu",n);
+    mpz_clear(n);
     for (u_int n = 0;n < digits;n++){
 	f = false;
-	for (u_int m = 0;m < 6;m++){
-	    if (num[n] == flippables[m]){
+	for (u_int m = 0;m < 7;m++){
+	    if (b[n] == flippables[m]){
+		f = true;
 		break;
 	    }
 	}
@@ -23,5 +42,60 @@ bool is_flippable(u_int num[],u_int digits){
 	    return false;
 	}
     }
-    return true;
+    return 1;
+}
+
+// Returns whether or not str matches the criteria.
+bool is_found(char *str){
+    u_llong val;
+    sscanf(str,"%llu",&val);
+    if (val && val % 2011 == 0 && str[0] != '0' && is_flippable(str)){
+	return true;
+    }
+    return false;
+}
+
+// Returns the string that represents the first element that matches all the
+// criteria
+char *find_n(char **arr,u_int elemc){
+    char *ans;
+    char **next_arr = malloc(elemc * 6 * sizeof(char*));
+    for (int n = 0;n < elemc;n++){
+	if (is_found(arr[n])){
+	    ans = strdup(arr[n]);
+	    for (int m = 0;m < elemc;m++){
+		free(arr[m]);
+	    }
+	    free(arr);
+	    return ans;
+	}
+    }
+    for (int n = 0;n < elemc;n++){
+	for (int m = 0;m < 6;m++){
+	    next_arr[6 * n + m] = malloc((strlen(arr[n]) + 3) * sizeof(char));
+	    sprintf(next_arr[6 * n + m],"%c%s%c",
+		    pairs[m][0],arr[n],pairs[m][1]);
+	}
+	free(arr[n]);
+    }
+    free(arr);
+    return find_n(next_arr,elemc * 6);
+}
+
+int main(){
+    char **seed = malloc(12 * sizeof(char*));
+    seed[0]  = strdup("0");
+    seed[1]  = strdup("1");
+    seed[2]  = strdup("2");
+    seed[3]  = strdup("5");
+    seed[4]  = strdup("8");
+    seed[5]  = strdup("00");
+    seed[6]  = strdup("11");
+    seed[7]  = strdup("22");
+    seed[8]  = strdup("55");
+    seed[9]  = strdup("69");
+    seed[10] = strdup("88");
+    seed[11] = strdup("96");
+    printf("ANS: %s\n",find_n(seed,12));
+    return EXIT_SUCCESS;
 }
--- src/test.hs
+++ src/test.hs
@@ -1,30 +1,37 @@
 {-# LANGUAGE BangPatterns #-}
+
+-- Joe Jevnik
+-- 15.11.2013
 -- http://domino.research.ibm.com/Comm/wwwr_ponder.nsf/Challenges/February2011.html
 -- IBM feb 2011
 
 import Data.Char (digitToInt,intToDigit)
+import Data.Word
 
---is_flippable :: (Integral a,Show a,Read a) => a -> Bool
-is_flippable n = all (\d -> digitToInt d `elem` [1,2,5,6,8,9]) $ show n
+-- Returns True if n is a flippable number.
+-- Returns False if n is not flippable.
+is_flippable :: (Integral a,Show a,Read a) => a -> Bool
+is_flippable n = all (\d -> digitToInt d `elem` [0,1,2,5,6,8,9]) $ show n
 
---pairs :: (Integral a,Show a,Read a) => [(a,a)]
-pairs = [(1,1),(2,2),(5,5),(6,9),(8,8),(9,6)]
+-- A list of all of the pairs that can be used to make a flippable palindrome.
+pairs :: [(Word8,Word8)]
+pairs = [(0,0),(1,1),(2,2),(5,5),(6,9),(8,8),(9,6)]
 
---candidates :: (Integral a,Show a,Read a) => [[a]]
-candidates = bld [[1],[2],[5],[8],[11],[22],[55],[69],[88],[96]]
-  where
-      bld ns = ns ++ bld [a:n ++ [b] | n <- ns, (a,b) <- pairs]
-
-
---find_n :: (Integral a,Show a,Read a) => a
-find_n = find_n' [[1],[2],[5],[8],[11],[22],[55],[69],[88],[96]]
+-- Finds the number, returns it as a list of digirs.
+-- Seeds the recursion with the base list, then calls make larger lists by
+-- checking all possible palindromic flippables.
+-- WARNING: Does not terminate and uses ~7gb of ram after a bit of time.
+find_n :: IO [Word8]
+find_n = find_n' [[1],[2],[5],[8],[1,1],[2,2],[5,5],[6,9],[8,8],[9,6]]
   where
       find_n' !ns
-          | (not . null) (ms ns) = head (ms ns)
-          | otherwise = find_n' [a:n ++ [b] | n <- ns, (a,b) <- pairs]
+          | (not . null) (ms ns) = return $ head (ms ns)
+          | otherwise = print (length ns)
+                        >> find_n' [a:n ++ [b] | n <- ns, (a,b) <- pairs]
       ms !ns = filter is_found ns
       is_found !m = let n = (read . concatMap show) m
                    in n `rem` 2011 == 0 && is_flippable (n^2)
 
+-- Print the answer.
 main :: IO ()
-main = print find_n
+main = find_n >>= print
